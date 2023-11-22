0f186a69e5299aba3d95c51fe97dd9c1ecf5e748
gustavo ima <lucenagustavo404@gmail.com>
2023-11-22 16:38:28 -0300

    adding update and delete routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { Pool, QueryResult } from "pg";
+import { error, log } from "console";
 
 const app = express();
 const port = 3000;
@@ -26,15 +27,19 @@ function getProdutcts(req: Request, res: Response): void {
     res.status(200).json(results.rows);
   });
 }
-
+function getPedidos(req: Request, res: Response): void {
+  pool.query('SELECT * FROM pedidos', (error: Error, results: QueryResult) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+}
 function createProduct(req: Request, res: Response): void {
-  const product = {
-    "name": "Produto A",
-    "price": 50.00,
-    "stock_amount": 100
-  }
+  console.log(req);
+  
 
-  const { name, price, stock_amount } = product;
+  const { name, price, stock_amount } = req.body;
 
   // Verifica se a tabela products existe, caso não exista, cria
   pool.query('CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, price MONEY, stock_amount INT)', (error: Error) => {
@@ -53,49 +58,82 @@ function createProduct(req: Request, res: Response): void {
 }
 
 function cadastrarPedido(req: Request, res: Response): void {
-  const name = "João Agusto";
-  const address = "Paris";
-  const productId = 1;
+  const { name, productId, address } = req.body;
 
   // Cria a tabela 'products' se ela não existir
-  pool.query('CREATE TABLE IF NOT EXISTS pedidos (id SERIAL PRIMARY KEY, client VARCHAR(255) NOT NULL, product JSONB, address VARCHAR(255))', (error: Error) => {
-    if (error) {
-      throw error;
-    }
-  
-    // Seleciona o produto com base no ID
-    pool.query('SELECT id, name, price FROM products WHERE id = $1;', [productId], (error: Error, results: QueryResult) => {
+  pool.query(
+    'CREATE TABLE IF NOT EXISTS pedidos (id SERIAL PRIMARY KEY, client VARCHAR(255) NOT NULL, product JSONB, address VARCHAR(255))',
+    (error: Error) => {
       if (error) {
         throw error;
       }
 
-      // Obtém os resultados da consulta
-      const product = results.rows;
-
-      // Verifica se o produto foi encontrado
-      if (!product) {
-        res.status(404).json({ message: 'Produto não encontrado' });
-        return;
-      }
-
-      const data = [name, product, address];
-      pool.query('INSERT INTO pedidos (client, product, address) VALUES ($1, $2, $3);', data, (error: Error, results: QueryResult) => {
+      // Seleciona o produto com base no ID
+      pool.query('SELECT id, name, price FROM products WHERE id = $1;', [productId], (error: Error, results: QueryResult) => {
         if (error) {
           throw error;
         }
-        res.status(201).json(results.rows);
+
+        // Obtém os resultados da consulta
+        const product = results.rows[0]; // Assuming you want the first result
+
+        // Verifica se o produto foi encontrado
+        if (!product) {
+          res.status(404).json({ message: 'Produto não encontrado' });
+          return;
+        }
+
+        const data = [name, { id: product.id, name: product.name, price: product.price }, address];
+        pool.query(
+          'INSERT INTO pedidos (client, product, address) VALUES ($1, $2, $3);',
+          data,
+          (error: Error, results: QueryResult) => {
+            if (error) {
+              throw error;
+            }
+            res.status(201).json(results.rows);
+          }
+        );
       });
-    });
-  });
+    }
+  );
+}
+
+function deleteProduct(req:Request, res:Response):void {
+  const {id}= req.body
+  pool.query('DELETE FROM products WHERE id = $1;',[id],(error: Error, results:QueryResult)=>{
+    if(error){
+      throw error;
+    }
+    res.status(200).json(results.rowCount);
+  } )
+  
+}
+
+function updateProduct(req:Request, res:Response):void {
+  const {id,local, value}= req.body;
+  console.log(req.body);
+  
+  pool.query(`UPDATE products SET ${local} = $1 WHERE id = $2;`,[value, id],(error: Error, results:QueryResult)=>{
+    if(error){
+      throw error;
+    }
+    res.status(200).json(results);
+  } )
+  
 }
 
 
 
 
 
+
 app.get("/", getProdutcts);
-app.get("/products", createProduct);
-app.get("/pedidos", cadastrarPedido);
+app.get("/pedidos", getPedidos)
+app.post("/products", createProduct);
+app.post("/pedidos", cadastrarPedido);
+app.delete("/products", deleteProduct);
+app.put("/products", updateProduct)
 
 
 app.listen(port, () => {
